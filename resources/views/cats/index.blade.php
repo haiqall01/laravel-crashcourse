5fcf51c49cb2c3a8094c4b4d56546aac66ae4fd5
haiqall01 <haiqal2721@gmail.com>
2024-05-09 13:39:15 +0800

    added controller and create basic list

---
+++ resources/views/cats/index.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cat</title>
+</head>
+<body>
+   <h1>my cat name is laravel</h1>
+   
+   <table border="1">
+        <thead>
+            <tr>
+                <th>Image</th>
+                <th>Name</th>
+                <th>Breed</th>
+                <th>Age</th>
+                <th>Color</th>
+                <th>Action</th>
+            </tr>    
+        </thead>
+        <tbody>
+            @forelse ($cats as $cat)
+                <tr>
+                    <td>
+                        <img src="{{ $cat->image_url }}" alt="{{ $cat->name }}" width="100">
+                    
+                    </td>
+                    <td>{{ $cat->name }}</td>
+                    <td>{{ $cat->breed }}</td>
+                    <td>{{ $cat->age }}</td>
+                    <td>{{ $cat->color }}</td> 
+                    <td>
+                    
+                    </td>
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="6">No Cats Found</td>
+                </tr>
+            @endforelse
+        </tbody>
+   </table>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,14 @@
 <?php
-
+use App\Http\Controllers\CatController;
 use Illuminate\Support\Facades\Route;
 
 //Route::get('/', function () {
  //   return view('welcome');
 //});
+//route pass ke controller
+//Route::get('/', function () {
+ //   $name = 'Haiqal Zulmi';
+ //   return view('hello', compact('name'));
+//});
 
-Route::get('/', function () {
-    $name = 'Haiqal Zulmi';
-    return view('hello', compact('name'));
-});
+Route::get('/', [CatController::class, 'index']);
