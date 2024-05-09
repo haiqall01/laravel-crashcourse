879ae714d2aa9f7321dbce059b7db591d21d6121
haiqall01 <haiqal2721@gmail.com>
2024-05-09 11:08:50 +0800

    init

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+//Route::get('/', function () {
+ //   return view('welcome');
+//});
+
 Route::get('/', function () {
-    return view('welcome');
+    $name = 'Haiqal Zulmi';
+    return view('hello', compact('name'));
 });
